c61a369498d7dddd658cd3dd6bfaa3a07f807b9f
Oren <orengolan@gmail.com>
2017-05-30 05:46:20 -0700

    polyfills issue

--- src/lib/polyfills.js
+++ src/lib/polyfills.js
@@ -4,78 +4,82 @@
 
 import 'unfetch/polyfill'
 import Promise from 'promise-polyfill'
-if (!window.Promise) {
-	window.Promise = Promise
-}
 
-// Object.assign polyfill. works on IE 13> and Safari 9>
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
-if (typeof Object.assign != 'function') {
-	Object.assign = function(target, varArgs) {
-		// .length of function is 2
-		if (target == null) {
-			// TypeError if undefined or null
-			throw new TypeError('Cannot convert undefined or null to object')
-		}
+// check for window object before adding anything to it.
+if (typeof window !== "underfined") {
+	if (!window.Promise) {
+		window.Promise = Promise
+	}
 
-		const to = Object(target)
+	// Object.assign polyfill. works on IE 13> and Safari 9>
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+	if (typeof Object.assign != 'function') {
+		Object.assign = function(target, varArgs) {
+			// .length of function is 2
+			if (target == null) {
+				// TypeError if undefined or null
+				throw new TypeError('Cannot convert undefined or null to object')
+			}
 
-		for (let index = 1; index < arguments.length; index++) {
-			const nextSource = arguments[index]
+			const to = Object(target)
 
-			if (nextSource != null) {
-				// Skip over if undefined or null
-				for (let nextKey in nextSource) {
-					// Avoid bugs when hasOwnProperty is shadowed
-					if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-						to[nextKey] = nextSource[nextKey]
+			for (let index = 1; index < arguments.length; index++) {
+				const nextSource = arguments[index]
+
+				if (nextSource != null) {
+					// Skip over if undefined or null
+					for (let nextKey in nextSource) {
+						// Avoid bugs when hasOwnProperty is shadowed
+						if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+							to[nextKey] = nextSource[nextKey]
+						}
 					}
 				}
 			}
+			return to
 		}
-		return to
 	}
-}
 
-// TODO: This should work on IE 9>. I am not sure why it's not working on IE 11
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-if (!Object.keys) {
-  Object.keys = (function() {
-    'use strict';
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
+	// TODO: This should work on IE 9>. I am not sure why it's not working on IE 11
+	// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+	if (!Object.keys) {
+		Object.keys = (function() {
+			'use strict';
+			var hasOwnProperty = Object.prototype.hasOwnProperty,
+					hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+					dontEnums = [
+						'toString',
+						'toLocaleString',
+						'valueOf',
+						'hasOwnProperty',
+						'isPrototypeOf',
+						'propertyIsEnumerable',
+						'constructor'
+					],
+					dontEnumsLength = dontEnums.length;
 
-    return function(obj) {
-      if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
-        throw new TypeError('Object.keys called on non-object');
-      }
+			return function(obj) {
+				if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
+					throw new TypeError('Object.keys called on non-object');
+				}
 
-      var result = [], prop, i;
+				var result = [], prop, i;
 
-      for (prop in obj) {
-        if (hasOwnProperty.call(obj, prop)) {
-          result.push(prop);
-        }
-      }
+				for (prop in obj) {
+					if (hasOwnProperty.call(obj, prop)) {
+						result.push(prop);
+					}
+				}
 
-      if (hasDontEnumBug) {
-        for (i = 0; i < dontEnumsLength; i++) {
-          if (hasOwnProperty.call(obj, dontEnums[i])) {
-            result.push(dontEnums[i]);
-          }
-        }
-      }
-      return result;
-    };
-  }());
+				if (hasDontEnumBug) {
+					for (i = 0; i < dontEnumsLength; i++) {
+						if (hasOwnProperty.call(obj, dontEnums[i])) {
+							result.push(dontEnums[i]);
+						}
+					}
+				}
+				return result;
+			};
+		}());
+	}
 }
