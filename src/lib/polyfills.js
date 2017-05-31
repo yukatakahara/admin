a29f8d8deeeedaed90ff5c935a2cd18ab70f2d63
Oren <orengolan@gmail.com>
2017-05-30 21:41:10 -0700

    remove Object.assign

--- src/lib/polyfills.js
+++ src/lib/polyfills.js
@@ -10,35 +10,6 @@ if (typeof window !== "undefined") {
 		window.Promise = Promise
 	}
 
-	// Object.assign polyfill. works on IE 13> and Safari 9>
-	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
-	if (typeof Object.assign != 'function') {
-		Object.assign = function(target, varArgs) {
-			// .length of function is 2
-			if (target == null) {
-				// TypeError if undefined or null
-				throw new TypeError('Cannot convert undefined or null to object')
-			}
-
-			const to = Object(target)
-
-			for (let index = 1; index < arguments.length; index++) {
-				const nextSource = arguments[index]
-
-				if (nextSource != null) {
-					// Skip over if undefined or null
-					for (let nextKey in nextSource) {
-						// Avoid bugs when hasOwnProperty is shadowed
-						if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-							to[nextKey] = nextSource[nextKey]
-						}
-					}
-				}
-			}
-			return to
-		}
-	}
-
 	// TODO: This should work on IE 9>. I am not sure why it's not working on IE 11
 	// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 	if (!Object.keys) {
